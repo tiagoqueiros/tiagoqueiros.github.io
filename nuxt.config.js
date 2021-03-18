10477bb8377cec9fafba159d24ec3e46c1d613b7
Tiago Queirós <tiagoqueiros@users.noreply.github.com>
2021-03-18 00:02:54 +0000

    feat: update site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: "Tiago Queirós",
+    title: "Tiago Queirós ⚡️ Delivery Manager",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
