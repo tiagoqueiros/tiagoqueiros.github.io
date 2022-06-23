616202698829e5e0fcf692756e83fe25c08f476a
Tiago Queirós <tiagoqueiros@users.noreply.github.com>
2022-06-23 14:05:52 +0100

    feat: update job position

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template lang="md">
 # Multidisciplinary digital creator from Porto, Portugal.
 
-Currently working as Delivery Manager at <a href="https://pixelmatters.com" target="_blank">Pixelmatters</a>.
+Currently working as Product Owner at <a href="https://pixelmatters.com" target="_blank">Pixelmatters</a>.
 
 - 👨‍💻 <a href="https://github.com/tiagoqueiros" target="_blank">Github</a>
 - 💼 <a href="https://www.linkedin.com/in/tiagofaqueiros" target="_blank">LinkedIn</a>
