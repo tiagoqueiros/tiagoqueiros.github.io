2eccb396bf49b12b49509b32fc9eb39f15663528
tiagoqueiros <tiagoqueiros@users.noreply.github.com>
2023-06-11 19:48:26 +0100

    feat: copy update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="w-screen	h-screen flex items-center justify-center p-5">
       <div className="absolute max-w-full md:max-w-md lg:max-w-xl xl:max-w-3xl 2xl:max-w-7xl p-10 z-10 grid gap-2 2xl:gap-8 grid-cols-1">
         <h1>Hi, I&apos;m Tiago Queirós 👋</h1>
-        <p>A results-driven Product Manager with a strong background in software and web engineering, and know-how in digital product design - with more than 8 years in the industry.</p>
+        <p>A results-driven Product Owner with a strong background in software and web engineering, and know-how in digital product design - with more than 8 years in the industry.</p>
         <p>Currently working as Product Owner at <a href='https://pixelmatters.com' target='_blank'>Pixelmatters</a>.</p>
         <ul>
           <li>👨‍💻 <a href="https://github.com/tiagoqueiros" target="_blank">GitHub</a></li>
