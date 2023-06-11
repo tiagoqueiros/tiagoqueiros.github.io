89fd195f9c74132771f97fc7c1ea1812c41df4d7
tiagoqueiros <tiagoqueiros@users.noreply.github.com>
2023-06-11 12:08:54 +0100

    chore: apply more tailwind styles
    * chore: make website more compatible with 4k

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-import './page.scss'
 import SvgWave from "@/components/svg-wave";
 
 export default function Home() {
   return (
-    <main className="home__container">
-      <div className="home__content--fixed">
-        <div className="home__content-wrapper">
-            <h1>Multidisciplinary digital creator from Porto, Portugal.</h1>
-            <p>Currently working as Product Owner at <a href="https://pixelmatters.com" target="_blank">Pixelmatters</a>.</p>
-            <ul>
-                <li>👨‍💻 <a href="https://github.com/tiagoqueiros" target="_blank">GitHub</a></li>
-                <li>💼 <a href="https://www.linkedin.com/in/tiagofaqueiros" target="_blank">LinkedIn</a></li>
-                <li>🐦 <a href="https://twitter.com/tiagofaqueiros" target="_blank">Twitter</a></li>
-            </ul>
-        </div>
+    <main className="w-screen	h-screen flex items-center justify-center p-5">
+      <div className="absolute max-w-xl p-10 z-10 grid gap-4 grid-cols-1 2xl:max-w-5xl 2xl:gap-8">
+        <h1>Multidisciplinary digital creator from Porto, Portugal.</h1>
+        <p>Currently working as Product Owner at <a href="https://pixelmatters.com" target="_blank">Pixelmatters</a>.</p>
+        <ul>
+          <li>👨‍💻 <a href="https://github.com/tiagoqueiros" target="_blank">GitHub</a></li>
+          <li>💼 <a href="https://www.linkedin.com/in/tiagofaqueiros" target="_blank">LinkedIn</a></li>
+          <li>🐦 <a href="https://twitter.com/tiagofaqueiros" target="_blank">Twitter</a></li>
+        </ul>
       </div>
-      <div className="home__content">
+      <div className="relative top-0 left-0 z-0 h-full rounded-2xl overflow-hidden">
           <SvgWave />
       </div>
     </main>
