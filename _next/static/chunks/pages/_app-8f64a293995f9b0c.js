1f98cf7e1b9b7b785f54affc3d45ad0737ec035c
tiagoqueiros <tiagoqueiros@users.noreply.github.com>
2025-04-22 18:57:52 +0000

    Deploying to main from @ tiagoqueiros/tiagoqueiros.github.io@26fd0ebb9f21d9fb080d9f983ed6bcc47546cc4f 🚀

---
+++ _next/static/chunks/pages/_app-8f64a293995f9b0c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{9540:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(4356)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(9540),n(6402))),_N_E=_.O()}]);
